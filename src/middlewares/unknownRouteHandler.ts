d2aad786fa0fd2ed204b82f2cf68f7a0c8ee3885
anryverse <anry.it.dev@gmail.com>
2026-04-30 11:19:24 +0800

    refactor: lint compliance by removing unused parameters

--- src/middlewares/unknownRouteHandler.ts
+++ src/middlewares/unknownRouteHandler.ts
@@ -1,10 +1,6 @@
-import { Request, Response, NextFunction } from 'express';
 import { AppError } from './errorHandler';
-
 export const unknownRouteHandler = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
+
 ) => {
   throw new AppError("Unknown Route", 404);
 };
